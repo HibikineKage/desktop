72e55ebbf77b84ada139057711636d15923cc9b2
Hibikine Kage <goodsmile314159@gmail.com>
2018-01-20 20:17:24 +0900

    複数ファイルから日付を選ばせるようにした
    年を4桁に修正

--- onedraw.py
+++ onedraw.py
@@ -7,6 +7,9 @@ v0.0.2
 複数ファイルを渡したときにうまく動作しない不具合を修正
 月や日付が1桁のときに名前が短くなってしまう不具合を修正
 onedrawディレクトリが存在しない時にエラーになる不具合を修正
+v0.0.3
+複数ファイルで日付が異なる場合、日付を選ばせるようにした
+年を4桁にした
 """
 
 import sys
@@ -15,7 +18,16 @@ import datetime
 import shutil
 from pathlib import Path
 
-version = '0.0.2'
+version = '0.0.3'
+
+def is_diff_day_exist(dts):
+    dt = dts[0]
+    # 違う時間が混ざってた場合はエラーにする
+    for x in dts:
+        if dt.year != x.year or dt.month != x.month or dt.day != x.day:
+            return True
+    return False
+             
 
 if __name__ == '__main__':
     print('onedraw.py v{}'.format(version))
@@ -42,14 +54,22 @@ if __name__ == '__main__':
         
     # 時間を取得
     dts = [datetime.datetime.fromtimestamp(ts) for ts in timestamps]
-    dt = dts[0]
-    # 違う時間が混ざってた場合はエラーにする
-    for x in dts:
-        if dt.year != x.year or dt.month != x.month or dt.day != x.day:
-            print('diffday exist!')
+    if is_diff_day_exist(dts):
+        print('diffday exist!')
+        print('please select true day! (-1 cancel)')
+        for i, [d, f] in enumerate(zip(dts, files)):
+            print('[{}]{}: {}'.format(i, f.name, d.strftime('%Y/%m/%d')))
+        value = int(input())
+        if value == -1:
+            print('stop moving files.')
+            exit(1)
+        try:
+            dt = dts[value]
+        except IndexError:
+            print('out of range!')
             exit(1)
 
-    new_dir_name = "{}_{}".format(dt.strftime('%y%m%d'), path.stem)
+    new_dir_name = "{}_{}".format(dt.strftime('%Y%m%d'), path.stem)
 
     onedraw_path = Path('onedraw')
     if not onedraw_path.exists():
